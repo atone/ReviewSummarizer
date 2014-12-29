09f9b7d962bfeedf57228a9e3c8340546af284a9
Naitong Yu <atone@126.com>
2014-12-29 13:54:43 +0800

    added some files

---
+++ src/edu/tsinghua/data/Review.java
@@ -0,0 +1,7 @@
+package edu.tsinghua.data;
+
+/**
+ * Created by FlyFish on 2014/12/29.
+ */
+public class Review {
+}
---
+++ src/edu/tsinghua/summarizer/Summarizer.java
@@ -0,0 +1,7 @@
+package edu.tsinghua.summarizer;
+
+/**
+ * Created by FlyFish on 2014/12/29.
+ */
+public class Summarizer {
+}
